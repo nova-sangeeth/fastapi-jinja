e563d9f1bd1ba229ccc5df6be472f4de8773d821
nova-sangeeth <novasangeeth@outlook.com>
2022-10-27 23:21:35 +0530

    application config.

---
+++ app/config.py
@@ -0,0 +1,7 @@
+APPLICATION_NAME = "FASTAPI JINJA"
+APPLICATION_DESCRIPTION = """
+Fastapi backend with support for Jinja along with 
+react components baked into the jinja templates."""
+
+
+DEBUG = True
