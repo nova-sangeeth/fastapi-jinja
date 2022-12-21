63b923015dfad95aacd36d8b69812597ff3cebcb
nova-sangeeth <novasangeeth@outlook.com>
2022-12-22 03:53:36 +0530

    format using black.

--- python_program_parser/py_code_parser.py
+++ python_program_parser/py_code_parser.py
@@ -6,6 +6,7 @@ Check if there are additional imports being added
 """
 
 import re
+
 data = {
     "version_id": "7",
     "row_id": "29",
@@ -21,11 +22,11 @@ data = {
     "txt_fib_solution_html": None,
     "numeric_solution": None,
     "question_options": None,
-    "form_builder_data": "[{\"type\": \"number\", \"required\": false, \"label\": \"Number\", \"placeholder\": \"Test\", \"className\": \"form-control\", \"name\": \"number-test\", \"value\": \"12\", \"min\": 10, \"max\": 12, \"step\": 12}]",
-    "params": "{\"number-test\": {\"type\": \"number\", \"value\": 12.0, \"min\": 10.0, \"max\": 12.0}}",
-    "py_code_data": "{\"code\": \"# add your imports here\\nfrom numpy.random import Generator\\n\\n\\nclass QuestionTemplate(AbstractQuestionTemplate):\\n\\n    @classmethod\\n    def validate_params(cls, params: ParametersModel) -> None:\\n        pass\\n\\n    @classmethod\\n    def generate(cls, params: ParametersModel, rng: Generator) -> BaseModel:\\n        pass\\n\\n    @classmethod\\n    def test_cases(cls, params: ParametersModel, question_model: BaseModel) -> None:\\n        pass\\n\", \"nonEditableLines\": [[0, 2], [3, 5], [5, 8], [9, 12], [13, 16]], \"methodLines\": [7, 11, 15]}",
+    "form_builder_data": '[{"type": "number", "required": false, "label": "Number", "placeholder": "Test", "className": "form-control", "name": "number-test", "value": "12", "min": 10, "max": 12, "step": 12}]',
+    "params": '{"number-test": {"type": "number", "value": 12.0, "min": 10.0, "max": 12.0}}',
+    "py_code_data": '{"code": "# add your imports here\\nfrom numpy.random import Generator\\n\\n\\nclass QuestionTemplate(AbstractQuestionTemplate):\\n\\n    @classmethod\\n    def validate_params(cls, params: ParametersModel) -> None:\\n        pass\\n\\n    @classmethod\\n    def generate(cls, params: ParametersModel, rng: Generator) -> BaseModel:\\n        pass\\n\\n    @classmethod\\n    def test_cases(cls, params: ParametersModel, question_model: BaseModel) -> None:\\n        pass\\n", "nonEditableLines": [[0, 2], [3, 5], [5, 8], [9, 12], [13, 16]], "methodLines": [7, 11, 15]}',
     "question_template_uuid": "a4a7a257fc0340ac93bfe9e93e46a23d",
-    "template_bank_id": "3"
+    "template_bank_id": "3",
 }
 
 class_definition = """import random
@@ -35,4 +36,3 @@ classmethod_decorator_definition = """def test_cases(cls, params: random.choice,
 
 
 python_data = data["py_code_data"]
-
